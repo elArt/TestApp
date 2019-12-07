57fe6311a5ed02284a7d76ef993a9cdd2e332ada
elArt <temka77777@gmail.com>
2019-12-07 19:43:15 +0300

    feature/make-layoutMackup layoutMarkup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-import Button from 'src/components/Button';
-import logo from './logo.svg';
-import './App.css';
-
+const { Header, Content, Footer } = Layout;
 function App() {
-
-  const handlerBtn = () => {
-    console.log('Do not use arrow functions in render methods in the REACT!!!!!');
-  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       <Button title="Click on me" actionClick={handlerBtn} />
-      </header>
+      <Layout className="layout">
+        <Header>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[2]} style={{ lineHeight: '64px' }}>
+            <Menu.Item key="1">Home</Menu.Item>
+            <Menu.Item key="2">Upload</Menu.Item>
+          </Menu>
+        </Header>
+
+        <Content style={{ padding: '0 50px' }}>Content</Content>
+
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2019 Created by Ant UED</Footer>
+      </Layout>
     </div>
   );
 }
